dc2e769faa9aeb962bd4b198c986ca72387e1b6a
Daniel Newton <daniel@pyxhealth.com>
2022-02-12 19:34:45 -0600

    remove scaffolded page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import SnakeGame from './components/Snake/SnakeGame';
-import NonogramGame from './components/Nonogram/NonogramGame';
 import './App.scss';
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/snake">Snake</Link></li>
-            <li><Link to="/nonogram">Nonogram</Link></li>
           </ul>
         </nav>
       </header>
@@ -21,7 +19,6 @@ function App() {
         <Routes>
           <Route path="/" element={<h2>Here be games</h2>}/>
           <Route path="/snake" element={<SnakeGame/>}/>
-          <Route path="/nonogram" element={<NonogramGame/>}/>
         </Routes>
       </main>
     </div>
